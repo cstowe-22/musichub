bea34f07c42d79c61649d913f9a9106082936014
cstowe-22 <56417887+cstowe-22@users.noreply.github.com>
2022-01-19 22:09:04 -0500

    Changes

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.use("", function(request, response){
 });
 
 //..............Start the server...............................//
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Server started at http://localhost:'+port+'.')
 });
